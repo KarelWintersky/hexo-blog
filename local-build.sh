c3bba21e12244f860ff24a12b2dc836ed95a22d6
Karel Wintersky <karel.wintersky@gmail.com>
2017-08-31 18:22:52 +0300

    fixes

--- local-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hexo generate
-
-echo 'removing old content'
-rm -rf /srv/webhosts/localhost/localhost/blog/*
-
-echo 'copying new content'
-cp -r /srv/hexoblog/public/* /srv/webhosts/localhost/localhost/blog/ 
